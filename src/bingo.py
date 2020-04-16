d15801c3aa35d232c203f76994455bca58a40d63
Sebastian <sebasegundo@live.com.ar>
2020-04-16 15:14:24 -0300

    Modificada fila 1 del carton para verificar si test_filas funciona

--- src/bingo.py
+++ src/bingo.py
@@ -2,9 +2,10 @@
 #Los 1 representan celdas ocupadas en el carton.
 def carton():
     carton = (
-        (1,0,0,1,1,0,1,0,1),
-        (0,1,1,1,0,1,1,1,1),
-        (0,1,0,0,1,0,0,1,0),
+        #Modifico el carton para dejar una fila completamente vacia y asi probar si test_filas funciona bien
+        (0,0,0,0,0,0,0,0,0),
+        (1,1,1,1,1,1,1,1,1),
+        (0,1,0,1,1,0,1,1,1),
     )
     return carton
 

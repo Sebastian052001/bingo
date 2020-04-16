378dc32a553a905c464077581b655a58a7bd74a7
Sebastian <sebasegundo@live.com.ar>
2020-04-16 11:46:42 -0300

    Agregado test que verifica que no haya columnas vacias

--- src/bingo.py
+++ src/bingo.py
@@ -1,5 +1,9 @@
-def Saludo():
-    return "Hola, que tal?"
+def carton():
+    carton = (
+        (1,0,0,1,1,0,1,0,1),
+        (0,1,1,1,0,1,1,1,1),
+        (0,1,0,0,1,0,0,1,0),
+    )
+    return carton
 
-def sumar(a,b):
-    return a + b
+print(carton())
---
+++ tests/test_celdas.py
@@ -0,0 +1,32 @@
+from src.bingo import carton
+
+
+def test_contar_celdas_ocupadas():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador == 15
+
+def test_celdas_menor_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador >= 15
+
+def test_celdas_mayor_15():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    assert contador <= 15
+
+def test_columna():
+    mi_carton = carton()
+    for x in range(0,8): 
+        assert mi_carton[0][x]+mi_carton[1][x]+mi_carton[2][x] > 0
+

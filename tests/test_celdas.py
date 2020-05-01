9bc92e5fb249e570cdcf1102033b99b81caa15e3
Sebastian <sebasegundo@live.com.ar>
2020-05-01 20:10:54 -0300

    Modificado validar_quince_numeros para funcionar con las nuevas pautas y movido a bingo.py

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,13 +1,13 @@
 from src.bingo import carton
-
+from src.bingo import validar_quince_numeros
 
 def test_contar_celdas_ocupadas():
-    mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-    assert contador == 15
+   mi_carton = (
+       (1,0,1,0,0,1,0,1,1),
+       (1,1,0,1,1,0,1,0,1),
+       (1,0,0,0,1,0,0,1,1),
+   )
+   assert validar_quince_numeros(mi_carton) == True
 
 def test_celdas_menor_15():
     mi_carton = carton()

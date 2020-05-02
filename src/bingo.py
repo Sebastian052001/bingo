228a07cf9029fd8d6dee85b1c25258fc6a6c5fd3
Sebastian <sebasegundo@live.com.ar>
2020-05-02 16:04:54 -0300

    Probando si al hacer un nuevo commit se soluciona problema de import

--- src/bingo.py
+++ src/bingo.py
@@ -2,13 +2,13 @@
 #Los 1 representan celdas ocupadas en el carton.
 def carton():
     carton = (
-        #Modifico el carton para dejar una fila completamente vacia y asi probar si test_filas funciona bien
         (0,1,0,1,0,1,1,0,1),
         (1,0,1,1,1,0,1,1,0),
         (0,1,0,0,1,0,0,1,1),
     )
     return carton
 
+#Valida que haya quince celdas ocupadas en el carton
 def validar_quince_numeros(carton):
     celdas_vacias = 0
     for fila in carton:
@@ -17,4 +17,22 @@ def validar_quince_numeros(carton):
                 celdas_vacias =  celdas_vacias + 1
     return celdas_vacias == 12
 
+#Valida que la cantidad de celdas ocupadas en el carton no sea menor a 15
+def validar_no_menor_a_15(carton):
+    celdas_vacias = 0
+    for fila in carton:
+        for celda in fila:
+            if celda == 0:
+                celdas_vacias = celdas_vacias + 1
+    return celdas_vacias <= 12
+
+#Valida que la cantidad de celdas ocupadas en el carton no sea mayor a 15
+def validar_no_mayor_a_15(carton):
+    celdas_vacias = 0
+    for fila in carton:
+        for celda in fila:
+            if celda == 0:
+                celdas_vacias = celdas_vacias + 1
+    return celdas_vacias >= 12
+    
 print(carton())
--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,5 +1,7 @@
 from src.bingo import carton
 from src.bingo import validar_quince_numeros
+from src.bingo import validar_no_menor_a_15
+from src.bingo import validar_no_mayor_a_15
 
 def test_contar_celdas_ocupadas():
    mi_carton = (
@@ -9,21 +11,22 @@ def test_contar_celdas_ocupadas():
    )
    assert validar_quince_numeros(mi_carton) == True
 
-def test_celdas_menor_15():
-    mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-    assert contador >= 15
+def test_cantidad_no_menor_a_quince():
+    mi_carton = (
+       (1,0,1,0,0,1,0,1,1),
+       (1,1,0,1,1,0,1,0,1),
+       (1,0,0,0,1,0,0,1,1),
+   )
+   assert validar_no_menor_a_15(mi_carton) == True
 
-def test_celdas_mayor_15():
-    mi_carton = carton()
-    contador = 0
-    for fila in mi_carton:
-        for celda in fila:
-            contador = contador + celda
-    assert contador <= 15
+def test_cantidad_no_mayor_a_quince():
+    mi_carton = (
+       (1,0,1,0,0,1,0,1,1),
+       (1,1,0,1,1,0,1,0,1),
+       (1,0,0,0,1,0,0,1,1),
+   )
+   assert validar_no_mayor_a_15(mi_carton) == True
+
 
 #comprueba que ninguna de las columnas del carton esten absolutamente vacias
 def test_columna():

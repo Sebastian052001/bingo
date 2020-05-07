463aa8bbba38e3bc7b9cd4b2d8f4674d349c850d
Sebastian <sebasegundo@live.com.ar>
2020-05-07 14:49:07 -0300

    Agregado test_avance_columnas

--- tests/test_avance.py
+++ tests/test_avance.py
@@ -1,5 +1,6 @@
 from src.bingo import carton
 from src.bingo import validar_avance_filas
+from src.bingo import validar_avance_columnas
 
 def test_avance_filas():
     mi_carton = (
@@ -8,3 +9,11 @@ def test_avance_filas():
        (9,50,65,0,70,0,80,0,0),
    )
     assert validar_avance_filas(mi_carton) == True
+
+def test_avance_columnas():
+    mi_carton = (
+       (1,0,3,0,0,15,0,25,30),
+       (4,5,0,8,9,0,16,0,40),
+       (9,50,65,0,70,0,17,0,0),
+   )
+    assert validar_avance_columnas(mi_carton) == True

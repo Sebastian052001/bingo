a0caf36eea0e9094f004d7374df05172e231051e
Sebastian <sebasegundo@live.com.ar>
2020-06-19 20:25:52 -0300

    Unificados Funcion_Cartones.py y bingo.py

--- src/bingo.py
+++ src/bingo.py
@@ -1,3 +1,5 @@
+import math
+import random
 #Los 0 representan celdas vacias en el carton.
 #Los 1 representan celdas ocupadas en el carton.
 def carton():
@@ -173,6 +175,112 @@ def validar_dosocupadas_consecutivas(carton):
             if (carton[x][y] != 0) and (carton[x][y+1] != 0) and (carton[x][y+2] != 0):
                 bool_valido = False
     return bool_valido
-    
+
+#Se encarga de crear un carton que luego se sometera a los tests
+def intentoCarton():
+    contador = 0
+    carton = (
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    )
+    numerosCarton = 0
+
+    while numerosCarton < 15:
+        contador += 1
+        if contador == 50:
+            return intentoCarton()
+        numero = random.randint(1, 90)
+
+        columna = math.floor(numero / 10)
+        if columna == 9:
+            columna = 8
+        huecos = 0
+        for i in range(0,3): 
+            if carton[columna][i] == 0:
+                huecos += 1
+            if carton[columna][i] == numero:
+                huecos = 0
+                break
+        if huecos < 2:
+            continue
+
+        fila = 0
+        for j in range(0,3):
+            huecos = 0
+            for i in range(0,9):
+                if carton[i][fila] == 0:
+                    huecos += 1
+
+            if huecos < 5 or carton[columna][fila] != 0:
+                fila += 1
+            else:
+                break
+        if fila == 3:
+            continue
+
+        carton[columna][fila] = numero
+        numerosCarton += 1
+        contador = 0
+    for x in range(0,9):
+        huecos = 0
+        for y in range (0,3):
+            if carton[x][y] == 0:
+                huecos += 1
+        if huecos == 3:
+            return intentoCarton()
+
+    return carton
+
 #Muestra en pantalla el carton
-#print(carton())
+def imprimirCarton(carton):
+    print("\n")
+    for fila in range(0,3):
+        for columna in range(0,9):
+            print(carton[fila][columna], end = ' ')
+        print("\n")
+    print("\n")
+
+#Se encarga de transformar el carton que pasa intentoCarton a uno que pueda interpretarse por las funciones que ya creamos antes
+def transformar_carton(carton):
+    carton_interpretable= (
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0]
+    )
+    for x in range (0,3):
+        for y in range (0,9):
+            carton_interpretable[x][y] = carton[y][x]
+    return carton_interpretable
+
+#Corre todos los tests en el carton que se le pase y devuelve True si pasa todos ellos de forma satisfactoria
+def tests_carton(carton_transformado):
+    #Por motivos que desconozco al dejar un enter por cada or en el if de abajo el programa lo toma como un error de sintaxis por lo que me veo forzado a escribir todo en la misma linea
+    if (validar_quince_numeros(carton_transformado) != True) or (validar_no_menor_a_quince(carton_transformado) != True) or (validar_no_mayor_a_quince(carton_transformado) != True) or (validar_no_columnas_vacias(carton_transformado) != True) or (validar_no_filas_vacias(carton_transformado) != True) or (validar_uno_a_noventa(carton_transformado) != True) or (validar_avance_filas(carton_transformado) != True) or (validar_avance_columnas(carton_transformado) != True) or (validar_no_repetidos(carton_transformado) != True) or (validar_5_filas_ocupadas(carton_transformado) != True) or (validar_matrix(carton_transformado) != True) or (validar_columnas_ocupadas(carton_transformado) != True) or (validar_3_columnas(carton_transformado) != True) or (validar_dosvacias_consecutivas(carton_transformado) != True) or (validar_dosocupadas_consecutivas(carton_transformado) != True):
+        return False
+    else:
+        return True
+
+#Transforma cada carton generado para luego testearlo, hasta encontrar uno que cumpla con todos los criterios y devolverlo
+def carton_definitivo():
+    #contador = 0
+    while True:
+        #contador += 1
+        #print(contador)
+        carton_original = intentoCarton()
+        carton_transformado = transformar_carton(carton_original)
+        if tests_carton(carton_transformado) == True:
+            break
+    return carton_transformado
+
+def main():
+    imprimirCarton(carton_definitivo())
+
+if __name__ == '__main__':
+    main()

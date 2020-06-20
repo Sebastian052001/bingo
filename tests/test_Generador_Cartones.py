4313db24dbab30db4a3e2077811af729a09b426e
Sebastian <sebasegundo@live.com.ar>
2020-06-20 18:03:07 -0300

    Agregados tests para carton y carton_definitivo

--- tests/test_Generador_Cartones.py
+++ tests/test_Generador_Cartones.py
@@ -1,9 +1,21 @@
+from src.bingo import carton
 from src.bingo import intentoCarton
 from src.bingo import transformar_carton
 from src.bingo import pruebas_carton
 from src.bingo import carton_definitivo
 
 def test_longitud_carton():
+    bool_Longitud = True
+    mi_carton = carton()
+    if len(mi_carton) != 3:
+        bool_Longitud = False
+    else:
+        for x in range (0,3):
+            if len(mi_carton[x]) != 9:
+                bool_Longitud = False
+    assert bool_Longitud == True
+
+def test_longitud_carton_intentoCarton():
     mi_carton = intentoCarton()
     bool_Longitud = True
     if len(mi_carton) != 9:
@@ -32,3 +44,14 @@ def test_evaluaciones_carton():
         [1,0,29,35,0,54,0,0,84]
     )    
     assert pruebas_carton(carton_transformado) == True
+
+def test_carton_defintivo():
+    bool_Longitud = True
+    mi_carton = carton_definitivo()
+    if (len(mi_carton) != 3):
+        bool_Longitud = False
+    else:
+        for x in range(0,3):
+            if(len(mi_carton[x]) != 9):
+                bool_Longitud = False
+    assert bool_Longitud == True
